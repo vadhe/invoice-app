c08f05acd7b90b61b2281529b845aa71ef88ba9f
vadhe <aldyvadhe@gmail.com>
2023-12-24 13:26:02 +0800

    feat: create component sidebar

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,7 @@ func main() {
 
 	http.Handle("/", templ.Handler(component))
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("dist"))))
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	fmt.Println("Listening on http://localhost:3000")
 	http.ListenAndServe(":3000", nil)
 }
